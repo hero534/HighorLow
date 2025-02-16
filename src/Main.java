d2d4f531bfdbdf661079f38326baca3f20e22a1d
hgold <goldsmhr@mail.uc.edu>
2025-02-16 13:43:13 -0500

    Task 04 Completed

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Random rnd = new Random();
         Scanner in = new Scanner(System.in);
-        int compInt = 0;
+        int compInt = rnd.nextInt(10)+1;
         int humanInt = 0;
-
+            do {
+                System.out.println("Enter a number between 1 and 10: ");
+                humanInt = in.nextInt();
+                    if (humanInt < compInt)
+                        System.out.println("Your guess was low!");
+                    else if (humanInt > compInt)
+                        System.out.println("Your guess was high!");
+                    else System.out.println("Your guess was on the money!");
+            }while(humanInt >= 1 && humanInt <= 10);
     }
 }
