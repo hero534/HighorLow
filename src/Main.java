5e3a6f1dbe8e9d50aa129803dae323bd75411b54
hgold <goldsmhr@mail.uc.edu>
2025-02-16 13:19:01 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
+        int compInt = 0;
+        int humanInt = 0;
+
+    }
+}
